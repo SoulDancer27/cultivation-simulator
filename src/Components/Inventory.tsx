6f24db324bc9a75387ed3a83c77367cf644fbc84
SoulDancer27 <igorpanin10@gmail.com>
2023-01-21 08:48:47 +0300

    Inventory arrows and filters

--- src/Components/Inventory.tsx
+++ src/Components/Inventory.tsx
@@ -2,30 +2,64 @@ import { Box, Typography, useTheme } from "@mui/material";
 import { isInventoryTreasure } from "GameConstants/Player";
 import PlayerContext from "GameEngine/Player/PlayerContext";
 import React from "react";
-import InventoryFilters from "./Inventory/InventoryFilters";
+import InventoryBottomPanel from "./Inventory/InventoryBottomPanel";
+import InventoryFiltersPane from "./Inventory/InventoryFilters";
 import InventoryGrid from "./Inventory/InventoryGrid";
 
-type InventoryFilters = "all" | "money" | "treasure";
+export type InventoryFilters = "all" | "money" | "treasure";
 
 export default function Inventory() {
   const theme = useTheme();
   const { inventory } = React.useContext(PlayerContext);
+  const [type, setType] = React.useState<InventoryFilters>("all");
   // Filter equipped items
-  const displayedInventory = inventory.filter((item) => {
+  let displayedInventory = inventory.filter((item) => {
     if (!isInventoryTreasure(item)) return true;
     else {
       return !item.isEquipped;
     }
   });
+
+  // Filter according to type
+  if (type !== "all") {
+    displayedInventory = displayedInventory.filter(
+      (item) => item.type === type
+    );
+  }
+
+  const [pages, setPages] = React.useState<InventoryPages>({
+    current: 1,
+    total: Math.ceil(displayedInventory.length / 36),
+  });
+
+  React.useEffect(() => {
+    setPages((pages) => {
+      const total = Math.ceil(displayedInventory.length / 36);
+      return { current: Math.min(pages.current, total), total };
+    });
+  }, [displayedInventory.length]);
+
+  // Select inventory for the page
+  const inventoryPage = displayedInventory.slice(
+    (pages.current - 1) * 36,
+    Math.min(pages.current * 36, displayedInventory.length)
+  );
+
   return (
     <Box paddingLeft={theme.spacing(1)}>
       <Typography variant="h5" marginTop={theme.spacing(2)}>
         Inventory
       </Typography>
       <Box display="flex">
-        <InventoryGrid inventory={displayedInventory} />
-        <InventoryFilters />
+        <InventoryGrid inventory={inventoryPage} />
+        <InventoryFiltersPane type={type} setType={setType} />
       </Box>
+      <InventoryBottomPanel pages={pages} setPages={setPages} />
     </Box>
   );
 }
+
+export type InventoryPages = {
+  current: number;
+  total: number;
+};
--- src/Components/Inventory/InventoryFilters.tsx
+++ src/Components/Inventory/InventoryFilters.tsx
@@ -1,5 +1,40 @@
-import { Box } from "@mui/material";
+import { Box, Button, useTheme } from "@mui/material";
+import { InventoryFilters } from "Components/Inventory";
 
-export default function InventoryFilters() {
-  return <Box width={128} height={512 - 128} bgcolor="purple"></Box>;
+type Props = {
+  type: InventoryFilters;
+  setType: React.Dispatch<React.SetStateAction<InventoryFilters>>;
+};
+
+export default function InventoryFiltersPane(props: Props) {
+  const { type, setType } = props;
+  const theme = useTheme();
+  return (
+    <Box width={128} height={512 - 128}>
+      <Button
+        variant="outlined"
+        onClick={() => setType("all")}
+        color={type === "all" ? "success" : "primary"}
+        fullWidth
+      >
+        all
+      </Button>
+      <Button
+        variant="outlined"
+        onClick={() => setType("money")}
+        color={type === "money" ? "success" : "primary"}
+        fullWidth
+      >
+        money
+      </Button>
+      <Button
+        variant="outlined"
+        onClick={() => setType("treasure")}
+        color={type === "treasure" ? "success" : "primary"}
+        fullWidth
+      >
+        treasures
+      </Button>
+    </Box>
+  );
 }
